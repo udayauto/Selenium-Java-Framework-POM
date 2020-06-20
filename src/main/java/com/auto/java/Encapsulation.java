db96374c19f07a614e8ddc1744bced52aced5870
udayauto <uday.kirun@gmail.com>
2020-06-20 15:24:24 -0400

    java

---
+++ src/main/java/com/auto/java/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.auto.java;
+
+public class Encapsulation {
+
+	private String medicine;
+
+	public void setmedicine(String medicine) {
+		this.medicine = medicine;
+	}
+
+	public String getmedicine() {
+		return this.medicine;
+	}
+
+}
---
+++ src/main/java/com/auto/java/Runner.java
@@ -0,0 +1,10 @@
+package com.auto.java;
+
+public class Runner {
+	public static void main(String[] args) {
+		Encapsulation capsule = new Encapsulation();
+		capsule.setmedicine("parasetamel");
+		System.out.println(capsule.getmedicine());
+
+	}
+}
